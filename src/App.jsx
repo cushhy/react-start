1082ba381b0ff666c232237a75f5640cb1b0f0a2
Тимур Семко <fred.kristov@gmail.com>
2025-04-13 22:07:30 +0300

    no jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import ReactLogo from './assets/react.svg?react';
+import { createElement } from 'react';
 
 // декларативный стиль
-export const App = () => (
-	<div>
-		<h1>Hello World</h1>
-		<ReactLogo />
-		<div>
-			<span>{new Date().getFullYear()}</span>
-		</div>
-	</div>
-);
+// export const App = () => (
+// 	<div>
+// 		<h1>Hello World</h1>
+// 		<ReactLogo />
+// 		<div>
+// 			<span>{new Date().getFullYear()}</span>
+// 		</div>
+// 	</div>
+// );
+
+export const App = () =>
+	createElement(
+		'div',
+		null,
+		createElement('h1', null, 'Hello World'),
+		createElement(ReactLogo),
+		createElement('div', null, createElement('span', null, new Date().getFullYear())),
+	);
